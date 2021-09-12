53ab2116aa1a4bc8dc9b5953f54bf30aececde91
SoumyaCy <56735635+SoumyaCy@users.noreply.github.com>
2021-09-12 20:59:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-var dice=["https://github.com/SoumyaCy/DIce-Game/tree/main/images/dice1.png","https://github.com/SoumyaCy/DIce-Game/tree/main/images/dice2.png","https://github.com/SoumyaCy/DIce-Game/tree/main/images/dice3.png","https://github.com/SoumyaCy/DIce-Game/tree/main/images/dice4.png","https://github.com/SoumyaCy/DIce-Game/tree/main/images/dice5.png","https://github.com/SoumyaCy/DIce-Game/tree/main/images/dice6.png"];
+var dice=["https://SoumyaCy.github.io/DIce-Game/images/dice1.png","https://SoumyaCy.github.io/DIce-Game/images/dice2.png","https://SoumyaCy.github.io/DIce-Game/images/dice3.png","https://SoumyaCy.github.io/DIce-Game/images/dice4.png","https://SoumyaCy.github.io/DIce-Game/images/dice5.png","https://SoumyaCy.github.io/DIce-Game/images/dice6.png"];
 function choosePick(){
   var num= Math.floor(Math.random()*6);
   var num1= Math.floor(Math.random()*6);
